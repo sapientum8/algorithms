b54780763bf3d983b04be57a6a0ada9494c7b4c1
sapientum8 <sapientum08@gmail.com>
2021-06-20 10:47:36 -0400

    problem p045 + solution

---
+++ src/main/daily/p045.py
@@ -0,0 +1,26 @@
+# Using a function rand5() that returns an integer from 1 to 5 (inclusive)
+# with uniform probability, implement a function rand7() that returns an integer
+# from 1 to 7 (inclusive).
+
+import random
+
+
+def rand5():
+    return int(random.randint(1,5))
+
+
+def rand7():
+    r = 21
+    while r > 20:
+        r = 5 * (rand5() - 1) + rand5() - 1
+    return int(r / 3) + 1
+
+
+n = [0] * 7
+
+for i in range(10000):
+    n[rand7()-1] += 1
+
+for i,v in enumerate(n):
+    print("{} => {}".format(i+1,v))
+
